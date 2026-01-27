d9c089a0347b2c0047ac7b203c8e1dd5b9ff12cf
Mrutyunjay Giri <mrutyunjaygiri470@gmail.com>
2026-01-27 23:31:08 +0530

    Implement sum calculation in sum.java

--- Day1/sum.java
+++ Day1/sum.java
@@ -1,7 +1,11 @@
 public class sum {
     public static void main(String[] args) {
-        int a
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
         
     }
     
 }
+
